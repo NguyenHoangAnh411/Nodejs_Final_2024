9f96f823986e38b103c62793e87823d976eeb236
KhoaNguyen210 <96481978+KhoaNguyen210@users.noreply.github.com>
2024-12-06 11:07:12 +0700

    Change Total

--- client/src/components/Cart/CartSummary.js
+++ client/src/components/Cart/CartSummary.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
-function CartSummary({ totalPrice, taxes, shippingFee, discount, totalPayment, onCheckout }) {
-  const formatNumber = (num) => (typeof num === 'number' ? num.toFixed(2) : '0.00');
+function CartSummary({ totalPrice, taxes, shippingFee, discount, onCheckout }) {
+  const validTotalPrice = totalPrice || 0;
+  const validTaxes = taxes || 0;
+  const validShippingFee = shippingFee || 0;
+  const validDiscount = discount || 0;
+
+  // Calculate total dynamically
+  const totalPayment =
+    validTotalPrice * 25000 +
+    validTaxes * 25000 +
+    validShippingFee * 25000 -
+    validDiscount * 25000;
+
+  const formatNumber = (num) => (typeof num === 'number' ? num.toLocaleString('vi-VN') : '0');
+
   return (
     <div>
       <h3>Order Summary</h3>
-      <p>Subtotal: {`${(totalPrice * 25000).toLocaleString('vi-VN')} VND`}</p>
-      <p>Taxes: {`${(taxes * 25000).toLocaleString('vi-VN')} VND`}</p>
-      <p>Shipping Fee: {`${(shippingFee * 25000).toLocaleString('vi-VN')} VND`}</p>
-      <p>Discount: {`${(discount * 25000).toLocaleString('vi-VN')} VND`}</p>
-      <h4>Total: {`${(totalPayment * 25000).toLocaleString('vi-VN')} VND`}</h4>
+      <p>Subtotal: {`${formatNumber(validTotalPrice * 25000)} VND`}</p>
+      <p>Taxes: {`${formatNumber(validTaxes * 25000)} VND`}</p>
+      <p>Shipping Fee: {`${formatNumber(validShippingFee * 25000)} VND`}</p>
+      <p>Discount: {`${formatNumber(validDiscount * 25000)} VND`}</p>
+      <h4>Total: {`${formatNumber(totalPayment)} VND`}</h4>
       <button onClick={onCheckout}>Checkout</button>
     </div>
   );
